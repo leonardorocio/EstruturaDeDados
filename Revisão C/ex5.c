aead097ad2999e55b8ecc61933b73210198c484d
Leonardo Fagundes Rócio <50716715+leonardorocio@users.noreply.github.com>
2023-08-31 19:23:29 -0300

    Update ex5.c

--- Revisão C/ex5.c
+++ Revisão C/ex5.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fatorial(int n) {
-	if (n <= 1) {
-		return 1;
-	} else {
-		return n * fatorial(n - 1);
-	}
-}
-
 int main() {
 	int n;
 	printf("Digite o total de degraus:\n");
 	scanf("%d", &n);
 	int fatoriais[n+1];
 	int i, ones = n, twos = 0, total = 0;
-	for (i = 0; i <= n; fatoriais[i] = fatorial(i), i++);
+	fatoriais[0] = 1;
+	for (i = 1; i <= n; fatoriais[i] = fatoriais[i-1] * i, i++);
 	while (ones >= 0) {
 		total += fatoriais[n] / (fatoriais[ones] * fatoriais[twos]);
 		n--;
